0121ef51fdc5d8383825dd828cfec6d8f9622588
Thoren Depner <thorendepner@gmail.com>
2023-06-01 16:56:50 +0200

    part 1-3 completed, 4 partially completed

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,18 @@
+class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+    @list = List.find(params[:list_id])
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to lists_path
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
